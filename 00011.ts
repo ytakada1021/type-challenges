181b7c6c2f795a3f8b7d48769eac2e6102d9f8c5
Yuki Takada <yuki.takada@ytakada.dev>
2024-08-19 17:08:17 +0900

    00011

---
+++ 00011.ts
@@ -0,0 +1,7 @@
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  [K in T[number]]: K;
+};
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+
+type result = TupleToObject<typeof tuple>; // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
