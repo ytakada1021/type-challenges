d747cc0aec568729665469983ec351ab09539a09
Yuki Takada <yuki.takada@ytakada.dev>
2024-08-20 01:14:12 +0900

    improve 00533

--- 00533.ts
+++ 00533.ts
@@ -1,7 +1,3 @@
-type Concat<T extends unknown[], P extends unknown[]> = T extends [...infer U]
-  ? P extends [...infer Q]
-    ? [...U, ...Q]
-    : never
-  : never;
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U];
 
 type Result = Concat<[1], [2]>; // expected to be [1, 2]
