1f4ddcb706180fccbfcee01ee3d8a5ad1ed0f9f8
Yuki Takada <yuki.takada@ytakada.dev>
2024-08-20 15:41:10 +0900

    add test case

--- 00014.ts
+++ 00014.ts
@@ -1,7 +1,51 @@
-type First<T extends [unknown, ...unknown[]]> = T[0];
+/*
+  14 - First of Array
+  -------
+  by Anthony Fu (@antfu) #easy #array
 
-type arr1 = ["a", "b", "c"];
-type arr2 = [3, 2, 1];
+  ### Question
 
-type head1 = First<arr1>; // expected to be 'a'
-type head2 = First<arr2>; // expected to be 3
+  Implement a generic `First<T>` that takes an Array `T` and returns its first element's type.
+
+  For example:
+
+  ```ts
+  type arr1 = ['a', 'b', 'c']
+  type arr2 = [3, 2, 1]
+
+  type head1 = First<arr1> // expected to be 'a'
+  type head2 = First<arr2> // expected to be 3
+  ```
+
+  > View on GitHub: https://tsch.js.org/14
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type First<T extends unknown[]> = T extends [infer First, ...infer Rest]
+  ? First
+  : never;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "./util-types";
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>
+];
+
+type errors = [
+  // @ts-expect-error
+  First<"notArray">,
+  // @ts-expect-error
+  First<{ 0: "arrayLike" }>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/14/answer
+  > View solutions: https://tsch.js.org/14/solutions
+  > More Challenges: https://tsch.js.org
+*/
--- 00043.ts
+++ 00043.ts
@@ -1,6 +1,44 @@
-type MyExclude<T, U extends T> = T extends U ? never : T;
+/*
+  43 - Exclude
+  -------
+  by Zheeeng (@zheeeng) #easy #built-in #union
 
-type Result = MyExclude<"a" | "b" | "c", "a">; // 'b' | 'c'
+  ### Question
+
+  Implement the built-in `Exclude<T, U>`
+
+  > Exclude from `T` those types that are assignable to `U`
+
+  For example:
+
+  ```ts
+  type Result = MyExclude<'a' | 'b' | 'c', 'a'> // 'b' | 'c'
+  ```
+
+  > View on GitHub: https://tsch.js.org/43
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type MyExclude<T, U> = T extends U ? never : T;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "./util-types";
+
+type cases = [
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a">, "b" | "c">>,
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a" | "b">, "c">>,
+  Expect<
+    Equal<MyExclude<string | number | (() => void), Function>, string | number>
+  >
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/43/answer
+  > View solutions: https://tsch.js.org/43/solutions
+  > More Challenges: https://tsch.js.org
+*/
 
 // ユニオン分配
 // https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
