abe261dd40ed5ac30382ac1e2e0ec41a140ebf87
Yuki Takada <yuki.takada@ytakada.dev>
2024-08-23 20:22:55 +0900

    21106

---
+++ medium/21106-medium-combination-key-type.ts
@@ -0,0 +1,33 @@
+/*
+  21106 - Combination key type
+  -------
+  by Nauxscript (@Nauxscript) #medium
+
+  ### Question
+
+  1. Combine multiple modifier keys, but the same modifier key combination cannot appear.
+  2. In the `ModifierKeys` provided, the priority of the previous value is higher than the latter value; that is, `cmd ctrl` is OK, but `ctrl cmd` is not allowed.
+
+  > View on GitHub: https://tsch.js.org/21106
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Combs<T extends string[]> = T extends [infer F extends string, ...infer R extends string[]]
+  ? `${F} ${R[number]}` | Combs<R>
+  : never;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "../util-types";
+
+type ModifierKeys = ["cmd", "ctrl", "opt", "fn"];
+type CaseTypeOne = "cmd ctrl" | "cmd opt" | "cmd fn" | "ctrl opt" | "ctrl fn" | "opt fn";
+
+type cases = [Expect<Equal<Combs<ModifierKeys>, CaseTypeOne>>];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/21106/answer
+  > View solutions: https://tsch.js.org/21106/solutions
+  > More Challenges: https://tsch.js.org
+*/
