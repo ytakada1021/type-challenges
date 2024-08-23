b19793416d43557c3e25a88b749901a4a8d8d43a
Yuki Takada <yuki.takada@ytakada.dev>
2024-08-23 12:57:38 +0900

    09286

---
+++ medium/09286-medium-firstuniquecharindex.ts
@@ -0,0 +1,51 @@
+/*
+  9286 - FirstUniqueCharIndex
+  -------
+  by jiangshan (@jiangshanmeta) #medium #string
+
+  ### Question
+
+  Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1. (Inspired by [leetcode 387](https://leetcode.com/problems/first-unique-character-in-a-string/))
+
+  > View on GitHub: https://tsch.js.org/9286
+*/
+
+/* _____________ Your Code Here _____________ */
+
+// if T includes S, return true
+type Includes<T extends string, U extends string> = T extends `${infer F}${infer R}`
+  ? F extends U
+    ? true
+    : Includes<R, U>
+  : false;
+
+type StringToTuple<S extends string> = S extends `${infer F}${infer R}`
+  ? [F, ...StringToTuple<R>]
+  : [];
+
+type FirstUniqueCharIndex<
+  T extends string,
+  S extends string = ""
+> = T extends `${infer F}${infer R}`
+  ? Includes<`${S}${R}`, F> extends true
+    ? FirstUniqueCharIndex<R, `${S}${F}`>
+    : StringToTuple<S>["length"]
+  : -1;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "../util-types";
+
+type cases = [
+  Expect<Equal<FirstUniqueCharIndex<"leetcode">, 0>>,
+  Expect<Equal<FirstUniqueCharIndex<"loveleetcode">, 2>>,
+  Expect<Equal<FirstUniqueCharIndex<"aabb">, -1>>,
+  Expect<Equal<FirstUniqueCharIndex<"">, -1>>,
+  Expect<Equal<FirstUniqueCharIndex<"aaa">, -1>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/9286/answer
+  > View solutions: https://tsch.js.org/9286/solutions
+  > More Challenges: https://tsch.js.org
+*/
