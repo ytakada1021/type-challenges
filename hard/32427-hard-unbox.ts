fcb2f2b43a99d6901759dd8a332a35c2f73c0c94
Yuki Takada <yuki.takada@ytakada.dev>
2024-08-28 15:57:07 +0900

    32427

---
+++ hard/32427-hard-unbox.ts
@@ -0,0 +1,105 @@
+/*
+  32427 - Unbox
+  -------
+  by Julian Coy (@eXamadeus) #hard #utils #promise #function #tuple #array
+
+  ### Question
+
+  How can we build a type that "unboxes" arrays, functions, promises, and tuples?
+
+  Example:
+
+  ```typescript
+  Unbox<string> // string
+  Unbox<()=>number> // number
+  Unbox<boolean[]> // boolean
+  Unbox<Promise<boolean>> // boolean
+  ```
+
+  Bonus: Can we make it recursive?
+
+  ```typescript
+  Unbox<() => () => () => () => number> // number
+  ```
+
+  Double Bonus: Can we control the recursion?
+
+  ```typescript
+  Unbox<() => () => () => () => number, 3> // () => number
+  ```
+
+  > View on GitHub: https://tsch.js.org/32427
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Unbox<T, N extends number = never, Counter extends unknown[] = []> = 0 extends N
+  ? Unbox<T>
+  : Counter["length"] extends N
+  ? T
+  : T extends (infer P)[]
+  ? Unbox<P, N, [...Counter, unknown]>
+  : T extends (...args: unknown[]) => infer Q
+  ? Unbox<Q, N, [...Counter, unknown]>
+  : T extends Promise<infer R>
+  ? Unbox<R, N, [...Counter, unknown]>
+  : T;
+
+// better solution in issues
+// type Unbox<T, N extends number = 0, Counter extends unknown[] = [unknown]> = T extends
+//   | (infer P)[]
+//   | ((...args: unknown[]) => infer P)
+//   | Promise<infer P>
+//   ? N extends Counter["length"]
+//     ? P
+//     : Unbox<P, N, [...Counter, unknown]>
+//   : T;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "../util-types";
+
+type cases = [
+  // Base cases
+  Expect<Equal<Unbox<number>, number>>,
+  Expect<Equal<Unbox<() => number>, number>>,
+  Expect<Equal<Unbox<() => number | string>, number | string>>,
+  Expect<Equal<Unbox<number[]>, number>>,
+  Expect<Equal<Unbox<(number | string)[]>, number | string>>,
+  Expect<Equal<Unbox<[number]>, number>>,
+  Expect<Equal<Unbox<Promise<number>>, number>>,
+
+  // Bonus: Recursion
+  Expect<Equal<Unbox<() => Promise<() => Array<Promise<boolean>>>>, boolean>>,
+
+  // Bonus: Recusion levels
+  Expect<Equal<Unbox<() => () => () => () => number, 0>, number>>,
+  Expect<Equal<Unbox<() => () => () => () => number, 1>, () => () => () => number>>,
+  Expect<Equal<Unbox<() => () => () => () => number, 2>, () => () => number>>,
+  Expect<Equal<Unbox<() => () => () => () => number, 3>, () => number>>,
+  Expect<Equal<Unbox<() => () => () => () => number, 4>, number>>,
+  Expect<Equal<Unbox<() => () => () => () => number, 5>, number>>,
+  Expect<Equal<Unbox<number[][][][], 0>, number>>,
+  Expect<Equal<Unbox<number[][][][], 1>, number[][][]>>,
+  Expect<Equal<Unbox<number[][][][], 2>, number[][]>>,
+  Expect<Equal<Unbox<number[][][][], 3>, number[]>>,
+  Expect<Equal<Unbox<number[][][][], 4>, number>>,
+  Expect<Equal<Unbox<number[][][][], 5>, number>>,
+  Expect<Equal<Unbox<[[[[number]]]], 0>, number>>,
+  Expect<Equal<Unbox<[[[[number]]]], 1>, [[[number]]]>>,
+  Expect<Equal<Unbox<[[[[number]]]], 2>, [[number]]>>,
+  Expect<Equal<Unbox<[[[[number]]]], 3>, [number]>>,
+  Expect<Equal<Unbox<[[[[number]]]], 4>, number>>,
+  Expect<Equal<Unbox<[[[[number]]]], 5>, number>>,
+  Expect<Equal<Unbox<Promise<Promise<Promise<number>>>, 0>, number>>,
+  Expect<Equal<Unbox<Promise<Promise<Promise<number>>>, 1>, Promise<Promise<number>>>>,
+  Expect<Equal<Unbox<Promise<Promise<Promise<number>>>, 2>, Promise<number>>>,
+  Expect<Equal<Unbox<Promise<Promise<Promise<number>>>, 3>, number>>,
+  Expect<Equal<Unbox<Promise<Promise<Promise<number>>>, 4>, number>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/32427/answer
+  > View solutions: https://tsch.js.org/32427/solutions
+  > More Challenges: https://tsch.js.org
+*/
