80ea1f4cc9e85cd50481a33c07d057b0c9e71b6f
Yuki Takada <yuki.takada@ytakada.dev>
2024-08-28 13:00:51 +0900

    03243

---
+++ medium/03243-medium-flattendepth.ts
@@ -0,0 +1,60 @@
+/*
+  3243 - FlattenDepth
+  -------
+  by jiangshan (@jiangshanmeta) #medium #array
+
+  ### Question
+
+  Recursively flatten array up to depth times.
+
+  For example:
+
+  ```typescript
+  type a = FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2> // [1, 2, 3, 4, [5]]. flattern 2 times
+  type b = FlattenDepth<[1, 2, [3, 4], [[[5]]]]> // [1, 2, 3, 4, [[5]]]. Depth defaults to be 1
+  ```
+
+  If the depth is provided, it's guaranteed to be positive integer.
+
+  > View on GitHub: https://tsch.js.org/3243
+*/
+
+/* _____________ Your Code Here _____________ */
+
+// type FlattenDepthRecursive<T extends unknown[], Remaining extends number> = T extends
+
+type Flatten<T extends unknown[]> = T extends [infer First, ...infer Rest]
+  ? First extends unknown[]
+    ? [...First, ...Flatten<Rest>]
+    : [First, ...Flatten<Rest>]
+  : [];
+
+type FlattenDepth<
+  T extends unknown[],
+  R extends number = 1,
+  Counter extends unknown[] = []
+> = Counter["length"] extends R
+  ? T
+  : T extends Flatten<T>
+  ? T
+  : FlattenDepth<Flatten<T>, R, [...Counter, unknown]>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "../util-types";
+
+type cases = [
+  Expect<Equal<FlattenDepth<[]>, []>>,
+  Expect<Equal<FlattenDepth<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<FlattenDepth<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, [[5]]]>>,
+  Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 3>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 19260817>, [1, 2, 3, 4, 5]>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/3243/answer
+  > View solutions: https://tsch.js.org/3243/solutions
+  > More Challenges: https://tsch.js.org
+*/
