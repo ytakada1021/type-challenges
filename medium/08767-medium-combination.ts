94c4533a377708a1368835e6004eef9e3d1d4b16
Yuki Takada <yuki.takada@ytakada.dev>
2024-08-30 02:36:36 +0900

    08767

---
+++ medium/08767-medium-combination.ts
@@ -0,0 +1,173 @@
+/*
+  8767 - Combination
+  -------
+  by Homyee King (@HomyeeKing) #medium #array #application #string
+
+  ### Question
+
+  Given an array of strings, do Permutation & Combination.
+  It's also useful for the prop types like video [controlsList](https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/controlsList)
+
+  ```ts
+  // expected to be `"foo" | "bar" | "baz" | "foo bar" | "foo bar baz" | "foo baz" | "foo baz bar" | "bar foo" | "bar foo baz" | "bar baz" | "bar baz foo" | "baz foo" | "baz foo bar" | "baz bar" | "baz bar foo"`
+  type Keys = Combination<['foo', 'bar', 'baz']>
+  ```
+
+  > View on GitHub: https://tsch.js.org/8767
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Permutation<R extends unknown[], L extends unknown[] = []> = R extends [
+  infer First,
+  ...infer Rest
+]
+  ?
+      | [First | "", ...Permutation<[...Rest, ...L]>]
+      | (Rest["length"] extends 0 ? never : Permutation<Rest, [...L, First]>)
+  : [];
+
+type Join<T extends string[]> = T extends [
+  infer First extends string,
+  ...infer Rest extends string[]
+]
+  ? Join<Rest> extends infer U
+    ? First extends First
+      ? U extends U
+        ? "" extends First | U
+          ? `${First}${Join<Rest>}`
+          : `${First} ${Join<Rest>}`
+        : never
+      : never
+    : never
+  : "";
+
+type Combination<T extends string[]> = Exclude<Join<Permutation<T>>, "">;
+
+// better solution in issues
+// type Combination<T extends string[], All = T[number], Item = All> = Item extends string
+//   ? Item | `${Item} ${Combination<[], Exclude<All, Item>>}`
+//   : never;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "../util-types";
+
+type cases = [
+  Expect<
+    Equal<
+      Combination<["foo", "bar", "baz"]>,
+      | "foo"
+      | "bar"
+      | "baz"
+      | "foo bar"
+      | "foo bar baz"
+      | "foo baz"
+      | "foo baz bar"
+      | "bar foo"
+      | "bar foo baz"
+      | "bar baz"
+      | "bar baz foo"
+      | "baz foo"
+      | "baz foo bar"
+      | "baz bar"
+      | "baz bar foo"
+    >
+  >,
+  Expect<
+    Equal<
+      Combination<["apple", "banana", "cherry"]>,
+      | "apple"
+      | "banana"
+      | "cherry"
+      | "apple banana"
+      | "apple cherry"
+      | "banana apple"
+      | "banana cherry"
+      | "cherry apple"
+      | "cherry banana"
+      | "apple banana cherry"
+      | "apple cherry banana"
+      | "banana apple cherry"
+      | "banana cherry apple"
+      | "cherry apple banana"
+      | "cherry banana apple"
+    >
+  >,
+  Expect<
+    Equal<
+      Combination<["red", "green", "blue", "yellow"]>,
+      | "red"
+      | "green"
+      | "blue"
+      | "yellow"
+      | "red green"
+      | "red blue"
+      | "red yellow"
+      | "green red"
+      | "green blue"
+      | "green yellow"
+      | "blue red"
+      | "blue green"
+      | "blue yellow"
+      | "yellow red"
+      | "yellow green"
+      | "yellow blue"
+      | "red green blue"
+      | "red green yellow"
+      | "red blue green"
+      | "red blue yellow"
+      | "red yellow green"
+      | "red yellow blue"
+      | "green red blue"
+      | "green red yellow"
+      | "green blue red"
+      | "green blue yellow"
+      | "green yellow red"
+      | "green yellow blue"
+      | "blue red green"
+      | "blue red yellow"
+      | "blue green red"
+      | "blue green yellow"
+      | "blue yellow red"
+      | "blue yellow green"
+      | "yellow red green"
+      | "yellow red blue"
+      | "yellow green red"
+      | "yellow green blue"
+      | "yellow blue red"
+      | "yellow blue green"
+      | "red green blue yellow"
+      | "red green yellow blue"
+      | "red blue green yellow"
+      | "red blue yellow green"
+      | "red yellow green blue"
+      | "red yellow blue green"
+      | "green red blue yellow"
+      | "green red yellow blue"
+      | "green blue red yellow"
+      | "green blue yellow red"
+      | "green yellow red blue"
+      | "green yellow blue red"
+      | "blue red green yellow"
+      | "blue red yellow green"
+      | "blue green red yellow"
+      | "blue green yellow red"
+      | "blue yellow red green"
+      | "blue yellow green red"
+      | "yellow red green blue"
+      | "yellow red blue green"
+      | "yellow green red blue"
+      | "yellow green blue red"
+      | "yellow blue red green"
+      | "yellow blue green red"
+    >
+  >,
+  Expect<Equal<Combination<["one", "two"]>, "one" | "two" | "one two" | "two one">>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/8767/answer
+  > View solutions: https://tsch.js.org/8767/solutions
+  > More Challenges: https://tsch.js.org
+*/
