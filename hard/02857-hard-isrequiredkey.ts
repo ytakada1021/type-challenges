4a05ffb481511271722474c2c2e956b1e049f322
Yuki Takada <yuki.takada@ytakada.dev>
2024-08-29 12:04:22 +0900

    02857

---
+++ hard/02857-hard-isrequiredkey.ts
@@ -0,0 +1,47 @@
+/*
+  2857 - IsRequiredKey
+  -------
+  by jiangshan (@jiangshanmeta) #hard #utils
+
+  ### Question
+
+  Implement a generic ```IsRequiredKey<T, K>```  that return whether ```K``` are required keys of ```T``` .
+
+  For example
+
+  ```typescript
+  type A = IsRequiredKey<{ a: number, b?: string },'a'> // true
+  type B = IsRequiredKey<{ a: number, b?: string },'b'> // false
+  type C = IsRequiredKey<{ a: number, b?: string },'b' | 'a'> // false
+  ```
+
+  > View on GitHub: https://tsch.js.org/2857
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type IsRequiredKey<T, K extends keyof T> = [K] extends [
+  keyof {
+    [P in keyof T as {} extends Pick<T, P> ? never : P]: T[P];
+  }
+]
+  ? true
+  : false;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "../util-types";
+
+type cases = [
+  Expect<Equal<IsRequiredKey<{ a: number; b?: string }, "a">, true>>,
+  Expect<Equal<IsRequiredKey<{ a: undefined; b: string }, "a">, true>>,
+  Expect<Equal<IsRequiredKey<{ a: number; b?: string }, "b">, false>>,
+  Expect<Equal<IsRequiredKey<{ a: number; b?: string }, "b" | "a">, false>>,
+  Expect<Equal<IsRequiredKey<{ a: undefined; b: undefined }, "b" | "a">, true>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2857/answer
+  > View solutions: https://tsch.js.org/2857/solutions
+  > More Challenges: https://tsch.js.org
+*/
